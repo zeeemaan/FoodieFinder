db63118d1a718fb87ac95de7b18238f0a28461d9
Peter Zamoranos <peter.i.zamoranos@gmail.com>
2018-11-02 20:54:22 -0500

    Added friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,105 @@
 //5. You should save your application's data inside of app/data/friends.js as an array of objects. Each of these objects should roughly follow the format below.
 
+var friends = [
 {
-  "name":"Ahmed",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  "scores":[
-      5,
-      1,
+  name: "Natalie",
+  photo: "https://www.imdb.com/name/nm0000204/mediaviewer/rm512132096",
+  scores:[
+      2,
       4,
+      3,
       4,
-      5,
-      1,
       2,
       5,
-      4,
+      3,
+      2,
+      5,
       1
     ]
-}
+},
+{
+  name: "Kaley",
+  photo: "https://www.imdb.com/name/nm0192505/mediaviewer/rm433818624",
+  scores:[
+      3,
+      1,
+      4,
+      5,
+      3,
+      4,
+      1,
+      1,
+      4,
+      5
+    ]
+},
+{
+  name: "Scarlett",
+  photo: "https://www.imdb.com/name/nm0424060/mediaviewer/rm1916122112",
+  scores:[
+      1,
+      5,
+      5,
+      4,
+      3,
+      2,
+      2,
+      5,
+      3,
+      4,
+    ]
+},
+{
+  name: "Gal",
+  photo: "https://www.imdb.com/name/nm2933757/mediaviewer/rm2906398720",
+  scores:[
+      4,
+      3,
+      2,
+      3,
+      1,
+      1,
+      4,
+      4,
+      3,
+      5,
+    ]
+},
+{
+  name: "Jennifer",
+  photo: "https://www.imdb.com/name/nm2225369/mediaviewer/rm2555577344",
+  scores:[
+      5,
+      5,
+      4,
+      2,
+      3,
+      3,
+      3,
+      5,
+      2,
+      3,
+    ]
+},
+{
+  name: "Emma",
+  photo: "https://www.imdb.com/name/nm1297015/mediaviewer/rm3228157440",
+  scores:[
+      4,
+      3,
+      2,
+      3,
+      1,
+      5,
+      4,
+      3,
+      1,
+      2,
+    ]
+  }
+];
+
+module.exports = friends;
+
+
+
